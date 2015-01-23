08ecbeb6a80b2fddb428bb80c8a987ebfb8f67e3
Alex Stanciu <alex.stanciu@coinzone.com>
2015-01-23 13:32:31 +0200

    change payment title

--- catalog/language/english/payment/coinzone.php
+++ catalog/language/english/payment/coinzone.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_title']				= 'Bitcoin - Coinzone';
+$_['text_title']				= 'Bitcoin - Powered by Coinzone';
 $_['text_wait']					= 'Please wait!';
 $_['text_loading']				= 'Loading';
 $_['text_error']                = 'There was an error communicating with the payment provider. Please try again later.';
